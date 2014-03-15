1abd9d6bdf2acb3a02bd6b15fb101c8f2ce0c2c7
Robin Murphy <robin@robinmurphy.co.uk>
2014-03-15 01:58:21 +0000

    Swicth to using stream events to handle incoming data

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 var ReadableStream = require('stream').Readable;
-var WritableStream = require('stream').Writable;
 var filesize = require('filesize');
 var colors = require('chalk');
 var zlib = require('zlib');
-var calculator = new WritableStream();
 
 function displayStats(original, compressed, ratio) {
   console.log(colors.yellow('Original size:'), colors.green(filesize(original)));
@@ -11,16 +9,22 @@ function displayStats(original, compressed, ratio) {
   console.log(colors.yellow('Compression ratio:'), colors.green((ratio * 100).toFixed(2) + '%'));
 }
 
-calculator._write = function (chunk, encoding) {
-  var gzipped = zlib.gzip(chunk, function (err, gzipped) {
-    var original = chunk.length;
-    var compressed = gzipped.length;
-    var ratio = compressed / original;
-
-    displayStats(original, compressed, ratio);
+function calculate(buffer) {
+  var gzipped = zlib.gzip(buffer, function (err, gzipped) {
+    if (err) throw err;
+    
+    displayStats(buffer.length, gzipped.length, buffer.length / gzipped.length);
   });
-};
+}
 
 module.exports.calculate = function (stream) {
-  stream.pipe(calculator);
+  var buffer = [];
+
+  stream.on('data', function (data) {
+    buffer.push(data);
+  });
+
+  stream.on('end', function () {
+    calculate(Buffer.concat(buffer));
+  });
 };
